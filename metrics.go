8088069887af0402057998d8241d3ce36fb48c2a
timothy <timothy@evsolutions.biz>
2025-01-16 07:42:48 +0200

    Add RTSP Metrics

---
+++ metrics.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+
+	// -- RTSP --
+
+	ClientConnBytesReceived = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "client_conn_bytes_received",
+		Help: "Total bytes received on the client connection.",
+	})
+
+	ClientConnBytesSent = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "client_conn_bytes_sent",
+		Help: "Total bytes sent on the client connection.",
+	})
+
+	SessionBytesReceived = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "session_bytes_received",
+		Help: "Total bytes received during the session.",
+	})
+
+	// -- RTP --
+
+	SessionRTPPacketsReceived = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "session_rtp_packets_received_total",
+		Help: "Total RTP packets received during the session.",
+	})
+
+	SessionRTPPacketsLost = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "session_rtp_packets_lost_total",
+		Help: "Total RTP packets lost during the session.",
+	})
+
+	SessionRTPPacketsInError = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "session_rtp_packets_in_error_total",
+		Help: "Total RTP packets in error during the session.",
+	})
+
+	SessionRTPJitter = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "session_rtp_jitter",
+		Help: "Average RTP jitter during the session.",
+	})
+
+	// -- RTCP --
+
+	SessionRTCPPacketsReceived = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "session_rtcp_packets_received_total",
+		Help: "Total RTCP packets received during the session.",
+	})
+
+	SessionRTCPPacketsInError = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "session_rtcp_packets_in_error_total",
+		Help: "Total RTCP packets in error during the session.",
+	})
+)
