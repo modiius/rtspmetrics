d58fff3de2e1f908460bea75f89f1100a54d5d5f
Timothée <42070232+modiius@users.noreply.github.com>
2025-01-19 21:16:16 +0200

    Add goroutine per RTSP session (#9)

--- metrics.go
+++ metrics.go
@@ -5,62 +5,88 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-var (
+type Metrics struct {
 	// -- Media --
-
-	FramesPerSecond = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "frames_per_second",
-		Help: "Frames per second of a video stream.",
-	})
+	FramesPerSecond prometheus.Gauge
 
 	// -- RTSP --
-
-	ClientConnBytesReceived = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "client_conn_bytes_received",
-		Help: "Total bytes received on the client connection.",
-	})
-
-	ClientConnBytesSent = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "client_conn_bytes_sent",
-		Help: "Total bytes sent on the client connection.",
-	})
-
-	SessionBytesReceived = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "session_bytes_received",
-		Help: "Total bytes received during the session.",
-	})
+	ClientConnBytesReceived prometheus.Gauge
+	ClientConnBytesSent     prometheus.Gauge
+	SessionBytesReceived    prometheus.Gauge
 
 	// -- RTP --
-
-	SessionRTPPacketsReceived = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "session_rtp_packets_received_total",
-		Help: "Total RTP packets received during the session.",
-	})
-
-	SessionRTPPacketsLost = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "session_rtp_packets_lost_total",
-		Help: "Total RTP packets lost during the session.",
-	})
-
-	SessionRTPPacketsInError = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "session_rtp_packets_in_error_total",
-		Help: "Total RTP packets in error during the session.",
-	})
-
-	SessionRTPJitter = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "session_rtp_jitter",
-		Help: "Average RTP jitter during the session.",
-	})
+	SessionRTPPacketsReceived prometheus.Counter
+	SessionRTPPacketsLost     prometheus.Counter
+	SessionRTPPacketsInError  prometheus.Counter
+	SessionRTPJitter          prometheus.Gauge
 
 	// -- RTCP --
+	SessionRTCPPacketsReceived prometheus.Counter
+	SessionRTCPPacketsInError  prometheus.Counter
+}
 
-	SessionRTCPPacketsReceived = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "session_rtcp_packets_received_total",
-		Help: "Total RTCP packets received during the session.",
-	})
+func NewMetrics(name, url string) *Metrics {
+	labelSet := prometheus.Labels{"name": name, "url": url}
 
-	SessionRTCPPacketsInError = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "session_rtcp_packets_in_error_total",
-		Help: "Total RTCP packets in error during the session.",
-	})
-)
+	return &Metrics{
+		FramesPerSecond: promauto.NewGauge(prometheus.GaugeOpts{
+			Name:        "frames_per_second",
+			Help:        "Frames per second of a video stream.",
+			ConstLabels: labelSet,
+		}),
+
+		ClientConnBytesReceived: promauto.NewGauge(prometheus.GaugeOpts{
+			Name:        "client_conn_bytes_received",
+			Help:        "Total bytes received on the client connection.",
+			ConstLabels: labelSet,
+		}),
+
+		ClientConnBytesSent: promauto.NewGauge(prometheus.GaugeOpts{
+			Name:        "client_conn_bytes_sent",
+			Help:        "Total bytes sent on the client connection.",
+			ConstLabels: labelSet,
+		}),
+
+		SessionBytesReceived: promauto.NewGauge(prometheus.GaugeOpts{
+			Name:        "session_bytes_received",
+			Help:        "Total bytes received during the session.",
+			ConstLabels: labelSet,
+		}),
+
+		SessionRTPPacketsReceived: promauto.NewCounter(prometheus.CounterOpts{
+			Name:        "session_rtp_packets_received_total",
+			Help:        "Total RTP packets received during the session.",
+			ConstLabels: labelSet,
+		}),
+
+		SessionRTPPacketsLost: promauto.NewCounter(prometheus.CounterOpts{
+			Name:        "session_rtp_packets_lost_total",
+			Help:        "Total RTP packets lost during the session.",
+			ConstLabels: labelSet,
+		}),
+
+		SessionRTPPacketsInError: promauto.NewCounter(prometheus.CounterOpts{
+			Name:        "session_rtp_packets_in_error_total",
+			Help:        "Total RTP packets in error during the session.",
+			ConstLabels: labelSet,
+		}),
+
+		SessionRTPJitter: promauto.NewGauge(prometheus.GaugeOpts{
+			Name:        "session_rtp_jitter",
+			Help:        "Average RTP jitter during the session.",
+			ConstLabels: labelSet,
+		}),
+
+		SessionRTCPPacketsReceived: promauto.NewCounter(prometheus.CounterOpts{
+			Name:        "session_rtcp_packets_received_total",
+			Help:        "Total RTCP packets received during the session.",
+			ConstLabels: labelSet,
+		}),
+
+		SessionRTCPPacketsInError: promauto.NewCounter(prometheus.CounterOpts{
+			Name:        "session_rtcp_packets_in_error_total",
+			Help:        "Total RTCP packets in error during the session.",
+			ConstLabels: labelSet,
+		}),
+	}
+}
